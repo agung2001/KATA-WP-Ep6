05cde2e461b8294e62a87fd8b0e18a6519d8ee90
Agung Sundoro <muhagung2001@gmail.com>
2023-08-20 14:41:59 +0700

    feat: add shortcode code sample

---
+++ template/container.php
@@ -0,0 +1,5 @@
+<div>
+    <h2><?php echo $atts['title']; ?></h2>
+    <p><?php echo $atts['description']; ?></p>
+    <?php echo do_shortcode($content); ?>
+</div>
--- youtube.php
+++ youtube.php
@@ -15,4 +15,49 @@
  * Domain Path:       /languages
  */
 
-// wp_enqueue_scripts
+/**
+ * YouTube shortcode
+ */
+function youtube_shortcode( $atts, $content = null ) {
+    // Simple shortcode
+    return 'Hello YouTube!';
+
+    // Shortcode with parameters
+    // return 'Hello YouTube! The video ID is ' . $atts['id'];
+
+    // Shortcode with default parameters
+//    $atts = shortcode_atts(
+//        array(
+//			'id'     => 'abc123',
+//			'width'  => 640,
+//			'height' => 480,
+//        ),
+//        $atts
+//    );
+
+    // Shortcode with parameters and content
+    // return 'Hello YouTube! The video ID is ' . $atts['id'] . '. The content is ' . $content;
+
+    // Shortcode with parameters and YouTube video
+    // return '<iframe width="' . $atts['width'] . '" height="' . $atts['height'] . '" src="https://www.youtube.com/embed/' . $atts['id'] . '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+}
+add_shortcode( 'youtube', 'youtube_shortcode' );
+
+
+/**
+ * YouTube container shortcode
+ */
+function youtube_container_shortcode( $atts, $content = null ) {
+    // Default shortcode parameters
+    $atts = shortcode_atts(
+        array(
+			'title'       => 'YouTube video',
+			'description' => 'Just another YouTube video',
+        ),
+        $atts
+    );
+
+    // Include container template
+    include plugin_dir_path( 'template/container.php' );
+}
+add_shortcode( 'youtube_container', 'youtube_container_shortcode' );
